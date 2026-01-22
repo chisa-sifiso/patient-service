6a342694290fc0edb006f1b7119e3f0487dbf4a9
chisa-sifiso <sifisovinjwa1@gmail.com>
2026-01-22 23:52:19 +0200

    adding the produter

---
+++ patient-services/src/main/java/za/ac/tut/patient_service/kafka/kafkaProducer.java
@@ -0,0 +1,14 @@
+package za.ac.tut.patient_service.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class kafkaProducer {
+    private final KafkaTemplate<String,byte[]> kafkaTemplate;
+
+
+   public kafkaProducer (KafkaTemplate<String,byte[]> kafkaTemplate){
+       this.kafkaTemplate=kafkaTemplate;
+   }
+}
